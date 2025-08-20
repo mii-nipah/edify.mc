ea57dd20474e776167c85e082747c6160866b363
nipah~✰! <mii_nipah@pm.me>
2025-08-20 18:14:41 -0300

    adds a second sub-pass for the scan, focused on non-supporting blocks, to allow for more realistic falling structures, instead of leaves and vines being able to keep everything from falling

--- src/main/kotlin/nipah/edify/GroupScan.kt
+++ src/main/kotlin/nipah/edify/GroupScan.kt
@@ -2,14 +2,12 @@ package nipah.edify
 
 import it.unimi.dsi.fastutil.longs.LongArrayFIFOQueue
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.LiquidBlock
 import nipah.edify.WorldData.chunkData
 import nipah.edify.utils.*
+import kotlin.coroutines.coroutineContext
 
 class GroupScan(
     val chunks: ChunkAccess,
@@ -17,9 +15,11 @@ class GroupScan(
     private val scanPerTick: Int = 10_000,
 ) {
     private val toVisit = LongArrayFIFOQueue(50_000)
+    private val toVisitWeak = LongArrayFIFOQueue(50_000)
     private val visited = LongOpenHashSet(1_000_000)
     private val group = LongOpenHashSet(300_000)
     private val metaGroup = LongOpenHashSet(300_000)
+    private val metaGroupWeak = LongOpenHashSet(300_000)
 
     fun clear() {
         toVisit.clear()
@@ -34,6 +34,7 @@ class GroupScan(
         clear()
         var matched = 0
         for (pos in seed) {
+            toVisitWeak.clear()
             toVisit.enqueue(pos.asLong())
             val ogGroupSize = group.size
             currentJob = mapGroup()
@@ -56,14 +57,14 @@ class GroupScan(
         return group.map { BlockPos.of(it) }
     }
 
-    private fun mapGroup() = TickScheduler.serverScope.launch {
-        fun inFoundation(pos: BlockPos): Boolean {
-            val chunk = chunks.at(pos) ?: return false
-            val lpos = chunk.worldToLocalPos(pos)
-            val cdata = chunkData[chunk.pos] ?: return false
-            return cdata.foundationAt(lpos.x, lpos.y, lpos.z)
-        }
+    private fun inFoundation(pos: BlockPos): Boolean {
+        val chunk = chunks.at(pos) ?: return false
+        val lpos = chunk.worldToLocalPos(pos)
+        val cdata = chunkData[chunk.pos] ?: return false
+        return cdata.foundationAt(lpos.x, lpos.y, lpos.z)
+    }
 
+    private fun mapGroup() = TickScheduler.serverScope.launch {
         var iter = 0
         var tickIter = toVisit.size()
 
@@ -87,6 +88,10 @@ class GroupScan(
             val chunk = chunks.at(pos) ?: continue
             val block = chunk.getBlockState(pos)
             if (block.isAir || block.isEmpty || block.block is LiquidBlock) continue
+            if (block.isNonSupporting()) {
+                toVisitWeak.enqueue(longPos)
+                continue
+            }
             val inFoundation = inFoundation(pos)
             visited.add(longPos)
             if (inFoundation.not()) {
@@ -102,6 +107,52 @@ class GroupScan(
                 toVisit.enqueue(longPos)
             }
         }
+        mapGroupWeak(iter, tickIter)
         group.addAll(metaGroup)
+    }
+
+    private suspend fun mapGroupWeak(startWithIters: Int, startWithTicks: Int) {
+        coroutineContext.ensureActive()
+
+        var iter = startWithIters
+        var tickIter = startWithTicks
+
+        metaGroupWeak.clear()
+
+        val pos = BlockPos.MutableBlockPos()
+        while (toVisitWeak.isNotEmpty() && coroutineContext.isActive) {
+            iter++
+            if (iter >= limit) {
+                return
+            }
+
+            tickIter++
+            if (tickIter > scanPerTick) {
+                tickIter = 0
+                nextServerTick()
+            }
+
+            val longPos = toVisitWeak.dequeueLong()
+            if (longPos in visited) continue
+            pos.set(longPos)
+            val chunk = chunks.at(pos) ?: continue
+            val block = chunk.getBlockState(pos)
+            if (block.isAir || block.isEmpty || block.block is LiquidBlock) continue
+            val inFoundation = inFoundation(pos)
+            visited.add(longPos)
+            if (inFoundation.not()) {
+                metaGroupWeak.add(longPos)
+            }
+            else {
+                return
+            }
+
+            pos.forEachNeighborNoAlloc { pos ->
+                val longPos = pos.asLong()
+                if (longPos in visited) return@forEachNeighborNoAlloc
+                toVisitWeak.enqueue(longPos)
+            }
+        }
+        group.addAll(metaGroupWeak)
     }
 }
